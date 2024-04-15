65e518b27d57af28d4a8b8933415acdef5e22237
Vorshim92 <vorshim92@gmail.com>
2024-04-15 14:14:24 +0200

    sql

---
+++ sql/gestione_libreria.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2024 at 02:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gestione_libreria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `libri`
+--
+
+CREATE TABLE `libri` (
+  `id` int(11) NOT NULL,
+  `titolo` varchar(255) NOT NULL,
+  `autore` varchar(255) NOT NULL,
+  `anno_pubblicazione` int(4) NOT NULL,
+  `genere` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `libri`
+--
+
+INSERT INTO `libri` (`id`, `titolo`, `autore`, `anno_pubblicazione`, `genere`) VALUES
+(1, 'L\'Isola del Tesoro', 'Robert Louis Stevenson', 1883, 'Romanzo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `libri`
+--
+ALTER TABLE `libri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `libri`
+--
+ALTER TABLE `libri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
